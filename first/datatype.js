d1b9a053b7df97e2f324eea2bba9c367ff7c0f66
ohmvikas <omtayde101@gmail.com>
2025-08-14 04:48:25 +0000

    datatype

---
+++ first/datatype.js
@@ -0,0 +1,10 @@
+let num = 234
+let string = "ohm vikas"
+let numberbig = BigInt(900000000000000000000000000000000000000)
+let boolean = true
+let name= null
+let distance;
+let mysymbol =Symbol("$one_time")
+
+console.log(typeof null)
+console.log(typeof undefined)
